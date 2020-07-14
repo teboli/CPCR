cea4b91fff4b4cf94b68ced3d880713500e35773
teboli <thomas.eboli@inria.fr>
2020-07-14 14:20:12 +0200

    cleaning training uniform and unifying blind and non-blind training

--- train_lchqs.py
+++ train_lchqs.py
@@ -15,7 +15,7 @@ import argparse
 import tqdm
 
 
-def train_greedy(loader, model, optimizer, criterion, epoch, d1, d2):
+def train_greedy(loader, model, optimizer, criterion, epoch, d1, d2, blind, noise_level):
     running_l1 = 0
     train_l1 = 0
     model.train(True)
@@ -32,7 +32,10 @@ def train_greedy(loader, model, optimizer, criterion, epoch, d1, d2):
         k = k.to(device)
         d = d.to(device)
 
-        nl = (12.75 - 0.5) * np.random.rand(1) + 0.5
+        if blind:
+            nl = (noise_level - 0.5) * np.random.rand(1) + 0.5
+        else:
+            nl = noise_level
         nl = float(nl) / 255
         y += nl*torch.randn_like(y)
         y = y.clamp(0, 1)
@@ -62,7 +65,7 @@ def train_greedy(loader, model, optimizer, criterion, epoch, d1, d2):
     return train_l1 / len(loader)
 
 
-def train_finetune(loader, model, optimizer, criterion, epoch, d1, d2):
+def train_finetune(loader, model, optimizer, criterion, epoch, d1, d2, blind, noise_level):
     running_l1 = 0
     train_l1 = 0
     model.train(False)
@@ -79,7 +82,10 @@ def train_finetune(loader, model, optimizer, criterion, epoch, d1, d2):
         k = k.to(device)
         d = d.to(device)
 
-        nl = (12.75 - 0.5) * np.random.rand(1) + 0.5
+        if blind:
+            nl = (noise_level - 0.5) * np.random.rand(1) + 0.5
+        else:
+            nl = noise_level
         nl = float(nl) / 255
         y += nl*torch.randn_like(y)
         y = y.clamp(0, 1)
@@ -109,7 +115,7 @@ def train_finetune(loader, model, optimizer, criterion, epoch, d1, d2):
     return train_l1 / len(loader)
 
 
-def validate(loader, model, epoch, d1, d2):
+def validate(loader, model, epoch, d1, d2, blind, noise_level):
     val_psnr = 0
     val_ssim = 0
     val_l1 = 0
@@ -121,7 +127,10 @@ def validate(loader, model, epoch, d1, d2):
     d2 = d2.expand(loader.batch_size, -1, -1, -1)
 
     # pre-create noise levels
-    nls = np.linspace(0.5, 12.75, len(loader))
+    if blind:
+        nls = np.linspace(0.5, noise_level, len(loader))
+    else:
+        nls = noise_level*np.ones(len(loader))
 
     with torch.no_grad():
         for i, data in tqdm.tqdm(enumerate(loader)):
@@ -152,14 +161,18 @@ if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--n_in', type=int, default=2)
-    parser.add_argument('--n_out', type=int, default=5)
-    parser.add_argument('--n_epochs', type=int, default=20)
+    parser.add_argument('--n_in', type=int, default=2, help='CPCR iterations; S in the paper.')
+    parser.add_argument('--n_out', type=int, default=5, help='HQS iterations; T in the paper.')
+    parser.add_argument('--n_epochs', type=int, default=20, help='Training epochs.')
     parser.add_argument('--batch_size', type=int, default=1)
-    parser.add_argument('--lr', type=float, default=3e-4)
-    parser.add_argument('--datasize', type=int, default=3000)
-    parser.add_argument('--ps', type=int, default=180)
-    parser.add_argument('--load_epoch', type=int, default=0)
+    parser.add_argument('--lr', type=float, default=1e-4)
+    parser.add_argument('--datasize', type=int, default=3000, help='Number of training samples.')
+    parser.add_argument('--ps', type=int, default=180, help='Size of training images.')
+    parser.add_argument('--load_epoch', type=int, default=0, help='Epoch to relead training; 0 is no loading.')
+    parser.add_argument('--noise_level', type=float, default=12.75)
+    parser.add_argument('--blind', type=int, default=1, help='Training on all noise levels between 0.5 and args.noise_level or only on args.noise_level')
+    parser.add_argument('--datapath', type=str, default='./data/training_uniform', help='Path of training samples.')
+    parser.add_argument('--lambd', type=float, default=1e-2, help='Weight for regularization in Eq.(10).')
 
     args = parser.parse_args()
 
@@ -171,56 +184,61 @@ if __name__ == '__main__':
     datasize = args.datasize
     ps = args.ps
     load_epoch = args.load_epoch
+    blind = args.blind
+    datapath = args.datapath
+    noise_level = args.noise_level
+    lambd = args.lambd
 
-    ### TRAIN
-
-    # data loader
-    images = '/sequoia/data1/teboli/irc_nonblind/data/'
-    datapath = '/sequoia/data2/teboli/irc_nonblind/data/training_uniform/'
-    savepath = '/sequoia/data1/teboli/irc_nonblind/results/training_uniform/'
-    savepath = os.path.join(savepath, 'net5_l1_aug_nout_%02d_nl_0.5to12.75' % (n_out))
+    ### data loader
+    datapath = '/sequoia/data2/teboli/irc_nonblind/data/training_uniform'
+    savepath = './results'
+    savepath = os.path.join(savepath, 'uniform_T_%02d_S_%02d' % (n_out, n_in))
+    if blind:
+        savepath += '_blind_0.5_to_%2.2f' % noise_level
+    else:
+        savepath += '_nonblind_%2.2f' % noise_level
     os.makedirs(savepath, exist_ok=True)
-    modelpath = '/sequioa/data2/teboli/irc_non_blind/models/training_uniform/'
-    modelpath = os.path.join(savepath, 'net5_l1_aug_nout_%02d_nin_%02d_ds_%05d_ps_%03d_nl_0.5to12.75_bs_%d' % (n_out, n_in, datasize, ps, batch_size))
+    modelpath = os.path.join(savepath, 'weights')
     os.makedirs(modelpath, exist_ok=True)
-    scorepath = '/sequioa/data1/teboli/irc_non_blind/models/training_uniform/'
-    scorepath = os.path.join(savepath, 'net5_l1_aug_nout_%02d_nin_%02d_ds_%05d_ps_%03d_nl_0.5to12.75_bs_%d.npy' % (n_out, n_in, datasize, ps, batch_size))
     dt_tr = datasets.UniformTrainDataset(datapath, datasize, ps, train=True, transform=True)
     dt_va = datasets.UniformTrainDataset(datapath, datasize, ps, train=False)
 
     loader_tr = DataLoader(dt_tr, batch_size=batch_size, shuffle=True, num_workers=4)
     loader_va = DataLoader(dt_va, batch_size=batch_size, shuffle=False, num_workers=4)
 
-    # model
-    model = networks.IRC_Net_v5(n_out, n_in)
-    criterion = loss.L1LossGreedy()
+
+
+
+    ######## STAGEWISE TRAINING ########
+    ### model
+    model = networks.LCHQS(n_out, n_in)
     if load_epoch > 0:
         filename = 'epoch_%03d.pt' % (load_epoch)
         state_dict_path = os.path.join(modelpath, filename)
         model.load_state_dict(torch.load(state_dict_path))
 
-    # inverse filters
+    ### inverse filters
     k1 = model.weight[0].data
-    d1 = kernels.compute_inverse_filter_basic(k1, 1e-2, 31).unsqueeze(0)
+    d1 = kernels.compute_inverse_filter_basic(k1, lambd, 31).unsqueeze(0)
     k2 = model.weight[1].data
-    d2 = kernels.compute_inverse_filter_basic(k2, 1e-2, 31).unsqueeze(0)
+    d2 = kernels.compute_inverse_filter_basic(k2, lambd, 31).unsqueeze(0)
 
-    # optimizer
+    ### optimizer
     optimizer = Adam(model.parameters(), lr=lr)
-    scheduler = StepLR(optimizer, step_size=n_epochs//2)
     model = model.to(device)
+    criterion = loss.L1LossGreedy()
     criterion = criterion.to(device)
     d1 = d1.to(device)
     d2 = d2.to(device)
 
-    # loop
+    ### loop
     scores = np.zeros((4, 2*n_epochs))
     if load_epoch < n_epochs:
         for epoch in range(load_epoch, n_epochs, 1):
             print('### Epoch %03d ###' % (epoch+1))
-            train_l1 = train_greedy(loader_tr, model, optimizer, criterion, epoch, d1, d2)
+            train_l1 = train_greedy(loader_tr, model, optimizer, criterion, epoch, d1, d2, blind, noise_level)
             print('   TR L1: %2.5f' % (train_l1))
-            val_psnr, val_ssim, val_l1 = validate(loader_va, model, epoch, d1, d2)
+            val_psnr, val_ssim, val_l1 = validate(loader_va, model, epoch, d1, d2, blind, noise_level)
             print('   VA L1: %2.5f / PSNR: %2.2f / SSIM: %2.3f' % (val_l1, val_psnr, val_ssim))
 
             # save net
@@ -236,31 +254,39 @@ if __name__ == '__main__':
             np.save(scorepath, scores)
 
 
-    ### FINETUNE
+
+
+    ######## END2END TRAINING ########
     loadpath = os.path.join(modelpath, 'epoch_%03d.pt' % n_epochs)
 
-    # model
-    model = networks.IRC_Net_v5(n_out, n_in)
+    ### model
+    model = networks.LCHQS(n_out, n_in)
     state_dict = torch.load(loadpath, map_location='cpu')
     model.load_state_dict(state_dict)
     if load_epoch > n_epochs:
         filename = 'epoch_%03d.pt' % (load_epoch)
         state_dict_path = os.path.join(modelpath, filename)
         model.load_state_dict(torch.load(state_dict_path))
-    criterion = nn.L1Loss()
 
-    # optimizer
+    ### inverse filters
+    k1 = model.weight[0].data
+    d1 = kernels.compute_inverse_filter_basic(k1, lambd, 31).unsqueeze(0)
+    k2 = model.weight[1].data
+    d2 = kernels.compute_inverse_filter_basic(k2, lambd, 31).unsqueeze(0)
+
+    ### optimizer
     lr = lr / 10
     optimizer = Adam(model.parameters(), lr=lr)
-    scheduler = StepLR(optimizer, step_size=n_epochs//2, gamma=0.2)
     model = model.to(device)
+    criterion = nn.L1Loss()
     criterion = criterion.to(device)
 
+    ### loop
     for epoch in range(max(n_epochs, load_epoch), 2*n_epochs):
         print('### Epoch %03d ###' % (epoch+1))
-        train_l1 = train_finetune(loader_tr, model, optimizer, criterion, epoch, d1, d2)
+        train_l1 = train_finetune(loader_tr, model, optimizer, criterion, epoch, d1, d2, blind, noise_level)
         print('   TR L1: %2.5f' % (train_l1))
-        val_psnr, val_ssim, val_l1 = validate(loader_va, model, epoch, d1, d2)
+        val_psnr, val_ssim, val_l1 = validate(loader_va, model, epoch, d1, d2, blind, noise_level)
         print('   VA L1: %2.5f / PSNR: %2.2f / SSIM: %2.3f' % (val_l1, val_psnr, val_ssim))
 
         # save net
